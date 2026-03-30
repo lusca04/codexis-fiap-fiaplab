3c4c3f2f5f3ffe31f78dfc0b86dcd44088a26b7d
Lusca04 <rodriguwalusca@gmail.com>
2026-03-30 18:41:51 -0300

    CREATING USER LOGIN SYSTEM

---
+++ Endpoints/Login.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+import sqlite3
+
+conn = sqlite3.connect("fiaplab.db")
+cursor = conn.cursor()
+
+app = Flask(__name__)
+
+@app.route("/Login/", methods=["POST"])
+def Login():
+    try:
+        Cpf = request.form.get("Cpf")
+        Rm = request.form.get("Rm")
+
+        if not all([Cpf, Rm]):
+                    return jsonify({"erro": "Cpf,Rm são obrigatórios."}), 400
+
+        cursor.execute("""
+            Select Cpf, Nome, Email, Rm, Ocupacao, Dt_Nascimento, Curso, 
+            Periodo, Dt_Formacao_Esperado, Dt_Cpu from Users
+            where Cpf = ? 
+            and Rm = ?
+            Limit 1
+            """, (Cpf, Rm))
+        
+        resultado = cursor.fetchone()
+        
+        if resultado:
+            usuario = {
+                "Cpf": resultado[0],
+                "Nome": resultado[1],
+                "Email": resultado[2],
+                "Rm": resultado[3],
+                "Ocupacao": resultado[4],
+                "Dt_Nascimento": resultado[5],
+                "Curso": resultado[6],
+                "Periodo": resultado[7],
+                "Dt_Formacao_Esperado": resultado[8],
+                "Dt_Cpu": resultado[9]
+            } 
+            return jsonify({"mensagem": f"Bem-Vindo {resultado[4]}:{resultado[1]}"}), 200
+        else:
+            return jsonify({"erro": "Usuário não cadastrado"}), 404
+    
+    except sqlite3.IntegrityError as e:
+        return jsonify({"erro": f"Erro ao buscar usúario em base: {str(e)}"}), 409
+
+    except Exception as e:
+        return jsonify({"erro": f": {str(e)}"}), 500
